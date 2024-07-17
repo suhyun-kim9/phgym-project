07fe85c3feeb0290680342c544c5c3e69396b619
hyeju <hyejux@gmail.com>
2024-07-17 14:51:48 +0900

    홈네비 수정함ㅋ

--- src/main/webapp/include/js/main-navigation.js
+++ src/main/webapp/include/js/main-navigation.js
@@ -12,3 +12,71 @@ var dropdownContent = document.querySelector(".dropdownContent");
   });
 
 
+  // var liElements = document.querySelectorAll('.main_nav > li');
+
+  // for (var i = 0; i<liElements.length; i++){
+  //   console.log(liElements[i]);
+  //   var target = liElements[i];
+  //   target.addEventListener("click", function(event){
+  //       event.target.classList.add('active'); 
+  //   });
+  // }
+
+
+
+  var li1 = document.querySelector(".main_nav > li:nth-of-type(1) > a");
+  var li2 = document.querySelector(".main_nav > li:nth-of-type(2) > a");
+  var li3 = document.querySelector(".main_nav > li:nth-of-type(3) > a");
+  var li4 = document.querySelector(".main_nav > li:nth-of-type(4) > a");
+  var li5 = document.querySelector(".main_nav > li:nth-of-type(5) > a");
+  var ani = document.querySelector(".animation");
+
+  li1.addEventListener("click",function(){
+    //li2.style.left = "340px";
+    ani.style.left = "200px";
+    ani.style.width = "130px";
+
+  })
+
+  li2.addEventListener("click",function(){
+    //li2.style.left = "340px";
+    ani.style.left = "357px";
+    ani.style.width = "115px";
+  })
+
+  li3.addEventListener("click",function(){
+    //li2.style.left = "340px";
+    ani.style.left = "492px";
+    ani.style.width = "150px";
+
+  })
+  li4.addEventListener("click",function(){
+    //li2.style.left = "340px";
+    ani.style.left = "660px";
+    ani.style.width = "110px";
+
+  })
+  
+  li5.addEventListener("click",function(){
+    //li2.style.left = "340px";
+    ani.style.left = "808px";
+    ani.style.width = "145px";
+
+  })
+
+
+
+
+//   const handleToggleActive = (e) => {
+//     const target = e.target;
+//     liElements.forEach((li) => {
+//       li.classList.remove('active');
+//     });
+//     target.classList.add('active');
+//   };
+
+
+  
+//   liElements.forEach((li) => {
+//     li.addEventListener('click', handleToggleActive);
+//   });
